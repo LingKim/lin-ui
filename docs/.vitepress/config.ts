7b99a0fe462eae9119b05335ffe5aa4c83566faf
lilin <1660312954@qq.com>
2023-12-09 17:09:54 +0800

    添加vitepress文档

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "lin-ui",
+  description: "组件库文档",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'basic',
+        items:[
+          {text:'Button',link:'/components/button'},
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
